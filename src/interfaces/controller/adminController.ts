0e387c6af6e286b78eec7f772bfbbf1688ce7620
sandeep <sandeepcheriyan@gmail.com>
2024-06-23 12:12:24 +0530

    adminSocket name issue fixed

--- src/interfaces/controller/adminController.ts
+++ src/interfaces/controller/adminController.ts
@@ -1,7 +1,7 @@
 import { Route, Req, Res, Next } from "../../entity/Types/ServerTypes";
 import venuModel from "../../frameworks/models/venueModel";
 import { UtilitySocket } from "../../usecases/utilitySocket";
-import { AdminSocket } from "../../usecases/adminS--ocket";
+import { AdminSocket } from "../../usecases/adminSocket";
 
 export class AdminController {
   constructor(
