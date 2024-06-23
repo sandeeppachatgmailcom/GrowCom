43d4959775a14ee61b48d5d32913036d1653bc13
sandeep <sandeepcheriyan@gmail.com>
2024-06-23 15:55:40 +0530

    promocode module updated

--- src/entity/usecases/commonUseCases/updatePromoCode.ts
+++ src/entity/usecases/commonUseCases/updatePromoCode.ts
@@ -1,3 +1,3 @@
 export interface UpdatePromoCodeUseCase {
-    updatePromoCode(data:{email:string,promoCode:string}):Promise<{status:boolean,message:string,role:string}|void >
-}
+    updatePromoCode(data: { email: string; promoCode: string; }): Promise<{ status: boolean; message: string; role: string; } | void>;
+  }
--- src/usecases/commonUseCases/updatePromoCodeSocket.ts
+++ src/usecases/commonUseCases/updatePromoCodeSocket.ts
@@ -1,39 +1,33 @@
 import { UserRepository } from "../../entity/repository/userRepository";
 import { UpdatePromoCodeUseCase } from "../../entity/usecases/commonUseCases/updatePromoCode";
 
-  class UpdatePromoCodeSocket implements UpdatePromoCodeUseCase{
-    constructor(
-        private userRepo : UserRepository,
-         
+class UpdatePromoCodeSocket implements UpdatePromoCodeUseCase {
+  constructor(
+    private userRepo: UserRepository,
+  ) {}
 
-    ){
-
-    } 
-    async updatePromoCode(data: { email: string; promoCode: string; }): Promise<{status: boolean; message: string; role: string; }| void > {
-      try {
-        console.log(data,'reached Socket')
-        let role = ''
-        if(data.promoCode == 'STD00001'){
-              role= 'student'
-        }
-        else if(data.promoCode == 'TRN00001'){
-              role = 'trainer'
-        }
-        if(role.length){
-            const validDate = new Date()
-            validDate.setDate(validDate.getDate() +30)
-            console.log(validDate,role,data.email,data.promoCode,'validDate,role,data.email,data.promoCode')
-            const result = await this.userRepo.applyPromocode( {email:data.email,promoCode:data.promoCode,role:role,validity:validDate})
-            return result
-        }
-        else{
-            return {status:false,message:'updateFailed',role:'user'}
-        }
+  async updatePromoCode(data: { email: string; promoCode: string; }): Promise<{ status: boolean; message: string; role: string; } | void> {
+    try {
+      console.log(data, 'reached Socket');
+      let role = '';
+      if (data.promoCode == 'STD00001') {
+        role = 'student';
+      } else if (data.promoCode == 'TRN00001') {
+        role = 'trainer';
+      }
+      if (role.length) {
+        const validDate = new Date();
+        validDate.setDate(validDate.getDate() + 30);
+        console.log(validDate, role, data.email, data.promoCode, 'validDate,role,data.email,data.promoCode');
+        const result = await this.userRepo.applyPromocode({ email: data.email, promoCode: data.promoCode, role: role, validity: validDate });
+        return result;
+      } else {
+        return { status: false, message: 'updateFailed', role: 'user' };
+      }
+    } catch (error) {
+      console.error(error);
     }
-    catch (error) {
-        
-    }
-      } 
+  }
 }
 
-export default UpdatePromoCodeSocket
+export default UpdatePromoCodeSocket;
