b0a4007a9c51b3e09a7fd55c14d43e6ee66b7705
sandeep <sandeepcheriyan@gmail.com>
2024-06-23 12:35:40 +0530

    mongoDbConnection updated

--- src/frameworks/webServer/mongoDB.ts
+++ src/frameworks/webServer/mongoDB.ts
@@ -3,7 +3,7 @@ import mongoD from "mongoose";
 
 class MongoDB {
     constructor() {
-        const link = "mongodb+srv://sandeeppachat:ZjaTYjiSj6wni0Ts@manGrow.ofsqurd.mongodb.net/mong?retryWrites=true&w=majority&appName=manGrow";
+        const link = "mongodb+srv://sandeeppachat:w6yGtOSj60IeUvXk@cluster0.s4hqvyg.mongodb.net/mong?retryWrites=true&w=majority";
       //  const link = 'mongodb://127.0.0.1:27017/mong'
         mongoD.connect(link)
             .then(() => {
